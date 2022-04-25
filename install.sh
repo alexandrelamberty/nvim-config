1edd0dbccf93afc97ce25bac4811f21aedc84850
Alexandre Lamberty <alexandrelamberty@gmail.com>
2022-04-25 16:26:38 +0200

    Adding -d to stow

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 
 PACKAGE=config
 TARGET=$HOME
+DIR="$(dirname "$0")"
 
 printf "Nvim configuration installation\n"
-stow -vDt "$TARGET" $PACKAGE
+stow -d "$DIR" -vDt "$TARGET" $PACKAGE
 rm -rf "$HOME/.config/nvim"
-stow -vSt "$TARGET" $PACKAGE
+stow -d "$DIR" -vSt "$TARGET" $PACKAGE
 
